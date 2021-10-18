74036de6580ef11f382190fd295f6e46b3d73276
steftes99 <stefanotescione83@gmail.com>
2021-10-18 10:23:55 +0200

    init

---
+++ index.php
@@ -0,0 +1,2 @@
+<!-- Esercizio di oggi: cartella/repo php-google-faq Riscrivere questa pagina del sito google https://policies.google.com/faq. 
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
