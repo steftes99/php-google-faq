ad21d4a3a40f6f9ab2f28fbd3e495027e974819b
steftes99 <stefanotescione83@gmail.com>
2021-10-18 10:32:47 +0200

    add cdn

--- index.php
+++ index.php
@@ -44,4 +44,23 @@ Ci sono diverse domande con relative risposte. Gestire il “Database” e la vi
     $languages = ['italiano', 'espanol','deutsch','dansk'];
 
     ?>
-    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- add css -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- add fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- add bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
